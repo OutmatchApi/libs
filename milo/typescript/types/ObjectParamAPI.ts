a76eb3fc1e862ccb6edc0a6a0f7a26396545e8db
generapiserviceaccount <genearapiyair@gmail.com>
2022-11-12 20:00:25 +0000

    committing generated code for: 966f6b0b-d21f-4626-9c72-fddf3f89d1e3

---
+++ milo/typescript/types/ObjectParamAPI.ts
@@ -0,0 +1,149 @@
+import { ResponseContext, RequestContext, HttpFile } from '../http/http';
+import { Configuration} from '../configuration'
+
+import { ApiApp } from '../models/ApiApp';
+import { ApiAppRequest } from '../models/ApiAppRequest';
+import { Language } from '../models/Language';
+import { Logo } from '../models/Logo';
+import { ModelError } from '../models/ModelError';
+import { Release } from '../models/Release';
+import { ReleaseRequest } from '../models/ReleaseRequest';
+import { Sdk } from '../models/Sdk';
+import { SdkRequest } from '../models/SdkRequest';
+import { SdkResponse } from '../models/SdkResponse';
+import { Spec } from '../models/Spec';
+import { User } from '../models/User';
+import { UserPostRequest } from '../models/UserPostRequest';
+import { UserPutRequest } from '../models/UserPutRequest';
+
+import { ObservableApiAppApi } from "./ObservableAPI";
+import { ApiAppApiRequestFactory, ApiAppApiResponseProcessor} from "../apis/ApiAppApi";
+
+export interface ApiAppApiCreateApiAppRequest {
+    /**
+     * Created api app object
+     * @type ApiAppRequest
+     * @memberof ApiAppApicreateApiApp
+     */
+    apiAppRequest?: ApiAppRequest
+}
+
+export interface ApiAppApiCreateApiAppReleaseRequest {
+    /**
+     * app id to associate the release with
+     * @type string
+     * @memberof ApiAppApicreateApiAppRelease
+     */
+    appId: string
+    /**
+     * Created release object
+     * @type ReleaseRequest
+     * @memberof ApiAppApicreateApiAppRelease
+     */
+    releaseRequest?: ReleaseRequest
+}
+
+export interface ApiAppApiCreateApiAppReleaseSdksRequest {
+    /**
+     * app id
+     * @type string
+     * @memberof ApiAppApicreateApiAppReleaseSdks
+     */
+    appId: string
+    /**
+     * release id
+     * @type string
+     * @memberof ApiAppApicreateApiAppReleaseSdks
+     */
+    releaseVersion: string
+    /**
+     * Created sdks objects
+     * @type SdkRequest
+     * @memberof ApiAppApicreateApiAppReleaseSdks
+     */
+    sdkRequest?: SdkRequest
+}
+
+export class ObjectApiAppApi {
+    private api: ObservableApiAppApi
+
+    public constructor(configuration: Configuration, requestFactory?: ApiAppApiRequestFactory, responseProcessor?: ApiAppApiResponseProcessor) {
+        this.api = new ObservableApiAppApi(configuration, requestFactory, responseProcessor);
+    }
+
+    /**
+     * Create an api app for the authenticated user
+     * Create api app
+     * @param param the request object
+     */
+    public createApiApp(param: ApiAppApiCreateApiAppRequest = {}, options?: Configuration): Promise<void> {
+        return this.api.createApiApp(param.apiAppRequest,  options).toPromise();
+    }
+
+    /**
+     * Create an api app release
+     * Create api app release
+     * @param param the request object
+     */
+    public createApiAppRelease(param: ApiAppApiCreateApiAppReleaseRequest, options?: Configuration): Promise<void> {
+        return this.api.createApiAppRelease(param.appId, param.releaseRequest,  options).toPromise();
+    }
+
+    /**
+     * Generate sdks for a relase
+     * Generate sdks for a relase
+     * @param param the request object
+     */
+    public createApiAppReleaseSdks(param: ApiAppApiCreateApiAppReleaseSdksRequest, options?: Configuration): Promise<void> {
+        return this.api.createApiAppReleaseSdks(param.appId, param.releaseVersion, param.sdkRequest,  options).toPromise();
+    }
+
+}
+
+import { ObservableUserApi } from "./ObservableAPI";
+import { UserApiRequestFactory, UserApiResponseProcessor} from "../apis/UserApi";
+
+export interface UserApiCreateUserRequest {
+    /**
+     * Created user object
+     * @type UserPostRequest
+     * @memberof UserApicreateUser
+     */
+    userPostRequest?: UserPostRequest
+}
+
+export interface UserApiUpdateUserRequest {
+    /**
+     * Update an existent user in the store
+     * @type UserPutRequest
+     * @memberof UserApiupdateUser
+     */
+    userPutRequest?: UserPutRequest
+}
+
+export class ObjectUserApi {
+    private api: ObservableUserApi
+
+    public constructor(configuration: Configuration, requestFactory?: UserApiRequestFactory, responseProcessor?: UserApiResponseProcessor) {
+        this.api = new ObservableUserApi(configuration, requestFactory, responseProcessor);
+    }
+
+    /**
+     * This can only be done by the logged in user.
+     * Create user
+     * @param param the request object
+     */
+    public createUser(param: UserApiCreateUserRequest = {}, options?: Configuration): Promise<User> {
+        return this.api.createUser(param.userPostRequest,  options).toPromise();
+    }
+
+    /**
+     * This can only be done by the logged in user.
+     * Update user
+     * @param param the request object
+     */
+    public updateUser(param: UserApiUpdateUserRequest = {}, options?: Configuration): Promise<void> {
+        return this.api.updateUser(param.userPutRequest,  options).toPromise();
+    }
+
+}
---
+++ milo/typescript/types/PromiseAPI.ts
@@ -0,0 +1,108 @@
+import { ResponseContext, RequestContext, HttpFile } from '../http/http';
+import { Configuration} from '../configuration'
+
+import { ApiApp } from '../models/ApiApp';
+import { ApiAppRequest } from '../models/ApiAppRequest';
+import { Language } from '../models/Language';
+import { Logo } from '../models/Logo';
+import { ModelError } from '../models/ModelError';
+import { Release } from '../models/Release';
+import { ReleaseRequest } from '../models/ReleaseRequest';
+import { Sdk } from '../models/Sdk';
+import { SdkRequest } from '../models/SdkRequest';
+import { SdkResponse } from '../models/SdkResponse';
+import { Spec } from '../models/Spec';
+import { User } from '../models/User';
+import { UserPostRequest } from '../models/UserPostRequest';
+import { UserPutRequest } from '../models/UserPutRequest';
+import { ObservableApiAppApi } from './ObservableAPI';
+
+import { ApiAppApiRequestFactory, ApiAppApiResponseProcessor} from "../apis/ApiAppApi";
+export class PromiseApiAppApi {
+    private api: ObservableApiAppApi
+
+    public constructor(
+        configuration: Configuration,
+        requestFactory?: ApiAppApiRequestFactory,
+        responseProcessor?: ApiAppApiResponseProcessor
+    ) {
+        this.api = new ObservableApiAppApi(configuration, requestFactory, responseProcessor);
+    }
+
+    /**
+     * Create an api app for the authenticated user
+     * Create api app
+     * @param apiAppRequest Created api app object
+     */
+    public createApiApp(apiAppRequest?: ApiAppRequest, _options?: Configuration): Promise<void> {
+        const result = this.api.createApiApp(apiAppRequest, _options);
+        return result.toPromise();
+    }
+
+    /**
+     * Create an api app release
+     * Create api app release
+     * @param appId app id to associate the release with
+     * @param releaseRequest Created release object
+     */
+    public createApiAppRelease(appId: string, releaseRequest?: ReleaseRequest, _options?: Configuration): Promise<void> {
+        const result = this.api.createApiAppRelease(appId, releaseRequest, _options);
+        return result.toPromise();
+    }
+
+    /**
+     * Generate sdks for a relase
+     * Generate sdks for a relase
+     * @param appId app id
+     * @param releaseVersion release id
+     * @param sdkRequest Created sdks objects
+     */
+    public createApiAppReleaseSdks(appId: string, releaseVersion: string, sdkRequest?: SdkRequest, _options?: Configuration): Promise<void> {
+        const result = this.api.createApiAppReleaseSdks(appId, releaseVersion, sdkRequest, _options);
+        return result.toPromise();
+    }
+
+
+}
+
+
+
+import { ObservableUserApi } from './ObservableAPI';
+
+import { UserApiRequestFactory, UserApiResponseProcessor} from "../apis/UserApi";
+export class PromiseUserApi {
+    private api: ObservableUserApi
+
+    public constructor(
+        configuration: Configuration,
+        requestFactory?: UserApiRequestFactory,
+        responseProcessor?: UserApiResponseProcessor
+    ) {
+        this.api = new ObservableUserApi(configuration, requestFactory, responseProcessor);
+    }
+
+    /**
+     * This can only be done by the logged in user.
+     * Create user
+     * @param userPostRequest Created user object
+     */
+    public createUser(userPostRequest?: UserPostRequest, _options?: Configuration): Promise<User> {
+        const result = this.api.createUser(userPostRequest, _options);
+        return result.toPromise();
+    }
+
+    /**
+     * This can only be done by the logged in user.
+     * Update user
+     * @param userPutRequest Update an existent user in the store
+     */
+    public updateUser(userPutRequest?: UserPutRequest, _options?: Configuration): Promise<void> {
+        const result = this.api.updateUser(userPutRequest, _options);
+        return result.toPromise();
+    }
+
+
+}
+
+
+
